833c015782c5198ebaaf59751c0ab742fab65f12
nazirul <nazirul.me01c@gmail.com>
2024-04-26 19:20:05 +0600

    environment variable added

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//tourismAdmin
-//vx4rqYQv5ZRbsdZe
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://tourismAdmin:vx4rqYQv5ZRbsdZe@cluster0.kygk2l2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kygk2l2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +25,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const TSpotsCollection = client.db("tourismDB").collection("touristSpots");
-    app.post("/touristSpots", async (req, res) => {
+
+    app.get("/touristspots", async (req, res) => {
+      const cursor = TSpotsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/touristspots", async (req, res) => {
       const newSpot = req.body;
 
       const result = await TSpotsCollection.insertOne(newSpot);
